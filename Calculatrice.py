29e3647eba3b309d9fb0f95d4a273508293ee123
BAYAlouisgaston <bayalouisgaston7@gmail.com>
2026-05-07 21:30:08 +0200

    Ajout des fonctions basic   de la structure d'une calculatrice

---
+++ Calculatrice.py
@@ -0,0 +1,10 @@
+def calculatrice():
+  # boite du  programme
+ continuer_logiciel = True 
+print("Bienvenue sur une calcultrice  basique.")
+# ajoute de  while pour la condition ( tant que )
+while continuer_logiciel:
+try:
+  
+  except ValueError:
+    print(Veuiller  entrer des chiffres  valide)
