78b0161ed528dd0822c04ddcddd97c68d7583624
BAYAlouisgaston <bayalouisgaston7@gmail.com>
2026-05-08 19:32:05 +0200

    Amélioration de la calculatrice.
    Refonte complète du code de la calculatrice et résolution des erreurs commises.

--- Calculatrice.py
+++ Calculatrice.py
@@ -1,10 +1,28 @@
 def calculatrice():
-  # boite du  programme
- continuer_logiciel = True 
-print("Bienvenue sur une calcultrice  basique.")
-# ajoute de  while pour la condition ( tant que )
-while continuer_logiciel:
-try:
-  
-  except ValueError:
-    print(Veuiller  entrer des chiffres  valide)
+  #boite du  programme
+  continuer_logiciel = True 
+  print("Bienvenue sur une calcultrice  basique.")
+  #ajout de while pour la condition 'tant que' 
+  while continuer_logiciel:
+    try:
+      #permettre a  l'utilisateur d'entrer des données
+      num1 = float(input("entrez votre  premier nombre"))
+      operateur = input("Entrez l'opérateur (+, -, *, /) : ")
+      num2 = float(input("entrez votre second nombre"))
+      if operateur == "+":
+        resulta = num1 + num2
+      elif operateur == "-":
+        resultat = num1 - num2
+      elif operateur == "*":
+            resultat = num1 * num2
+      elif operateur == "/":
+              if num2 == 0:
+                print("ERREUR : Division par zéro impossible.")
+                #utilisation de continue  pour   retourne  au debut.
+                continue
+                resulta =  num1/num2
+      else: 
+                  print("Erreur")
+    except ValueError:
+      print("veuillez entrez des formules correctes")  
+      #la logigue des calculs
